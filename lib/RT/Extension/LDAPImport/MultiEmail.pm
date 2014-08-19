08f3392eea7f2a155edc415fc57904e34bcb2993
Alex Vandiver <cpan@chmrr.net>
2014-08-19 21:33:26 +0000

    Import of ALEXMV/RT-Extension-LDAPImport-MultiEmail-0.04 from CPAN.
    gitpan-cpan-distribution: RT-Extension-LDAPImport-MultiEmail
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         ALEXMV/RT-Extension-LDAPImport-MultiEmail-0.04.tar.gz
    gitpan-cpan-author:       ALEXMV
    gitpan-cpan-maturity:     released

--- lib/RT/Extension/LDAPImport/MultiEmail.pm
+++ lib/RT/Extension/LDAPImport/MultiEmail.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package RT::Extension::LDAPImport::MultiEmail;
 
-our $VERSION = '0.02';
+our $VERSION = '0.04';
 
 {
     no warnings 'redefine';
@@ -19,20 +19,28 @@ sub _import_user {
     my $user = $self->SUPER::_import_user(@_);
     return unless $user;
 
-    my $attrname = RT->Config->Get('LDAPMultiEmail');
-    return unless $attrname;
+    my $secondary_attrs = RT->Config->Get('LDAPMultiEmail');
+    return unless $secondary_attrs;
+    $secondary_attrs = [ $secondary_attrs ] unless ref $secondary_attrs;
 
     $self->{seen}{$user->id}++;
 
     my $ldap = $args{ldap_entry};
     my $uid   = $ldap->get_value('uid');
     my $email = $ldap->get_value('mail');
-    my @secondary = grep {/\S/ and $_ ne $email} $ldap->get_value($attrname);
+
+    my %original_secondary;
+    my @secondary;
+    for my $attr (@{$secondary_attrs}) {
+        $original_secondary{$attr} = [ $ldap->get_value($attr) ];
+        push @secondary, map {[$attr, $_]} grep {/\S/ and $_ ne $email} @{$original_secondary{$attr}};
+    }
     my(@users, @merge);
-    for my $address (@secondary) {
+    for my $val (@secondary) {
+        my ($attrname, $address) = @{$val};
         my ($parsed) = Email::Address->parse($address);
         unless ($parsed) {
-            $self->_debug("Skipping alternate address $address, as it is invalid");
+            $self->_debug("Skipping alternate address $address in $attrname of $email, as it is invalid");
             next;
         }
 
@@ -51,7 +59,7 @@ sub _import_user {
                 my $other = RT::User->new( RT->SystemUser );
                 $other->Load( $effective_id->Content );
                 $self->_warn($user->EmailAddress." lists $address".
-                             " as a secondary, which is already merged into ".
+                             " in $attrname as a secondary, which is already merged into ".
                              $other->EmailAddress
                          );
                 next;
@@ -63,7 +71,12 @@ sub _import_user {
         # values in the remote LDAP store.
         $ldap->replace( uid  => $address );
         $ldap->replace( mail => $address );
-        $ldap->replace( $attrname => [] );
+        $ldap->replace( $_ => [] ) for @{$secondary_attrs};
+
+        # Even if we were told to "only update", we should be able to
+        # create aliases to merge in.
+        local $RT::LDAPUpdateUsers = $RT::LDAPUpdateOnly;
+        local $RT::LDAPUpdateOnly  = 0;
 
         # Build and import the secondary user
         my $data = $self->_build_user_object( ldap_entry => $ldap );
@@ -90,6 +103,8 @@ sub _import_user {
     for my $id (grep {not $merged{$_}} @{$user->GetMergedUsers->Content}) {
         my $alt = RT::User->new( RT->SystemUser );
         $alt->LoadOriginal( id => $id );
+        my ($effective_id) = $alt->Attributes->Named("EffectiveId");
+        next unless $effective_id->Creator == RT->SystemUser->id;
         $alt->UnMerge;
     }
 
@@ -97,7 +112,7 @@ sub _import_user {
     # scope shortly without being used again, but better safe than sorry.
     $ldap->replace( uid  => $uid );
     $ldap->replace( mail => $email );
-    $ldap->replace( $attrname => \@secondary );
+    $ldap->replace( $_ => $original_secondary{$_} ) for @{$secondary_attrs};
 
     return $user;
 }
@@ -110,17 +125,21 @@ RT-Extension-LDAPImport-MultiEmail - Import users with multiple email addresses 
 
 =over
 
-=item perl Makefile.PL
+=item C<perl Makefile.PL>
 
-=item make
+=item C<make>
 
-=item make install
+=item C<make install>
 
 May need root permissions
 
-=item Edit your /opt/rt4/etc/RT_SiteConfig.pm
+=item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
 
-Add this line:
+If you are using RT 4.2 or greater, add this line:
+
+    Plugin('RT::Extension::LDAPImport::MultiEmail');
+
+For RT 4.0, add this line:
 
     Set(@Plugins, qw(RT::Extension::LDAPImport::MultiEmail));
 
@@ -131,15 +150,30 @@ addresses, via:
 
     Set( $LDAPMultiEmail, 'alternateEmail' );
 
+Multiple alternate email address attributes can be specified using an
+array reference:
+
+    Set( $LDAPMultiEmail, ['alternateEmail', 'homeEmail'] );
+
 =back
 
 =head1 AUTHOR
 
-Alex Vandiver <alexmv@bestpractical.com>
+Best Practical Solutions, LLC E<lt>modules@bestpractical.comE<gt>
 
-=head1 LICENSE AND COPYRIGHT
+=head1 BUGS
 
-This software is Copyright (c) 2013 by Best Practical Solutions
+All bugs should be reported via email to
+
+    L<bug-RT-Extension-LDAPImport-MultiEmail@rt.cpan.org|mailto:bug-RT-Extension-LDAPImport-MultiEmail@rt.cpan.org>
+
+or via the web at
+
+    L<rt.cpan.org|http://rt.cpan.org/Public/Dist/Display.html?Name=RT-Extension-LDAPImport-MultiEmail>.
+
+=head1 COPYRIGHT
+
+This extension is Copyright (C) 20013-2014 Best Practical Solutions, LLC.
 
 This is free software, licensed under:
 
